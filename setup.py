73ed8d4822809a312de9dc439a22b09c89874dff
sai vamsi <saivamsi5764@gamil.com>
2025-12-17 11:22:34 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+from typing import List
+
+Hypen_e_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    This function returns the list of requirements
+    
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [req.replace("\n"," ")  for req in requirements]
+
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+
+    return requirements
+
+
+setup(
+
+    name="mlproject",
+    version="0.0.1",
+    author="VAMSI",
+    author_email="bommuvamsi24@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+    )
